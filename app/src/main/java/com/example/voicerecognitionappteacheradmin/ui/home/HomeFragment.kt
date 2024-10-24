6308819d206c975f5f7bf761779fe8cb08279e3b
kyun04 <efrencamposagrado0622@gmail.com>
2024-10-24 15:11:25 +0800

    improved auth and menu UI

--- app/src/main/java/com/example/voicerecognitionappteacheradmin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/voicerecognitionappteacheradmin/ui/home/HomeFragment.kt
@@ -7,7 +7,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.voicerecognitionappteacheradmin.DataClass.UsersClass
 import com.example.voicerecognitionappteacheradmin.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class HomeFragment : Fragment() {
 
@@ -16,6 +23,7 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,14 +33,53 @@ class HomeFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
+        auth = FirebaseAuth.getInstance()
+
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+//        val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+
+        val textview_teacher_display_name = binding.textViewTeacherDisplayName
+
+
+        if(auth.currentUser?.uid != null){
+            val firebase_uid = auth.currentUser?.uid.toString()
+            displayTeacherName(textview_teacher_display_name,firebase_uid)
         }
+
+
         return root
+    }
+
+    fun displayTeacherName(textview_teacher_displayname: TextView, fuid: String){
+
+        val dbref: DatabaseReference
+        dbref = FirebaseDatabase.getInstance().getReference("/users/")
+        dbref.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                val objects = snapshot.children.mapNotNull { child ->
+                    child.getValue(UsersClass::class.java)
+                }
+
+                for(dataRecords in objects){
+                    if(dataRecords.firebase_uid.equals(fuid) && dataRecords.role.equals("teacher")){
+                        var teacher_fullname = dataRecords.firstname+" "+dataRecords.lastname
+                        textview_teacher_displayname.setText(teacher_fullname)
+                    }
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+
     }
 
     override fun onDestroyView() {
